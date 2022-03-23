fb829ac6bf89e0de6d8a55f367d56d979f8b1461
Drevniy1981 <99173976+Drevniy1981@users.noreply.github.com>
2022-03-23 13:08:33 +0300

    Create 1981.js

---
+++ 1981.js
@@ -0,0 +1,20 @@
+  function lib() {
+        var _windowContainer = null,
+        _guid = null,
+        _poll = this,
+        _name = '',
+        _store = null;
+
+        var header, panel;
+
+        var save = function(){ //Функция сохранения изменений в анкете
+            if (!panel.container['form'].pharmId) return false;
+
+            var modifyData = '';
+
+            $.each(_store.getModifiedRecords(), function(index, rec){
+                $.each(rec.modified, function(name){
+                    modifyData += '&values['+index+']['+name+']=' + rec.data[name];
+                });
+                modifyData += '&id['+index+']=' + rec.id;
+            });
